8c1ac464285fe186c9ef5197f4b654633cf74945
Doug Sponsler <sponslerdb@gmail.com>
2018-07-18 13:38:56 -0400

    did a lot of stuff

--- R/metaxa2_proc.R
+++ R/metaxa2_proc.R
@@ -4,7 +4,7 @@
 #' @param tlev A string specifying the taxonomic level at which results should be aggregated
 #' @param rscore A number between 0 and 100 specifying the relaiability score cutoff; only reads having a reliability score >= \code{rscore} will pass filter
 #' @return A tibble of sample-wise taxonomic proportions
-metaxize <- function(x, tlev = "family", rscore = 60) {
+metaxize <- function(x, tlev = "family", rscore = 60, regext = "CT\\d+") {
   lev <- noquote(tlev)
   files <- list.files(path = x,
              full.names = TRUE,
@@ -22,7 +22,7 @@ metaxize <- function(x, tlev = "family", rscore = 60) {
       summarise(reads = n()) %>%
       mutate(prop = reads/sum(reads),
              taxon = str_remove(taxon, ".__")) %>%
-      mutate(sample = rep(str_extract(y, "CT\\d+"))) %>%
+      mutate(sample = rep(str_extract(y, regext))) %>%
       select(sample, taxon, prop)
   }
   )
@@ -33,53 +33,11 @@ metaxize <- function(x, tlev = "family", rscore = 60) {
 #'
 #' @param x,y Tibbles output from \code{metaxize}
 consensus <- function(x, y) {
-  inner_join(x, y, by = c("sample", "taxon"))
-}
-
-#'
-#'
-#'
-load_blast6 <- function(x) {
-  read_tsv(x,
-           col_names = c("qseqid", "sseqid", "pident",
-                         "length", "mismatch", "gapopen",
-                         "qstart", "qend", "sstart", "send",
-                         "evalue", "bitscore"),
-           guess_max = 100000) %>%
-    mutate(sample = rep(str_extract(x, "CT\\d+"))) %>%
-    separate("sseqid", c("a", "gi", "b", "sseqid", "z"), fill = "warn", sep = "\\|", remove = TRUE) %>%
-    select(sample, qseqid, sseqid, pident, length, gapopen, qstart, qend, sstart, send, evalue, bitscore) %>%
-    group_by(qseqid) %>%
-    slice(1)
-}
-
-#'
-#'
-#'
-load_all <- function(x) {
-  map(list.files(path = x,
-                 full.names = TRUE,
-                 ignore.case = TRUE),
-      load_blast6) %>%
-    bind_rows()
-}
-
-#'
-#'
-#'
-prepNCBI <- function() {
-  list(taxonomizr::read.nodes('~/metaxr/data/nodes.dmp'),
-       taxonomizr::read.names('~/metaxr/data/names.dmp'))
+  out <- inner_join(x, y, by = c("sample", "taxon"))
+  colnames(out) <- c("sample", "taxon",
+                     paste(deparse(substitute(x)), "prop", sep = "."),
+                     paste(deparse(substitute(y)), "prop", sep = "."))
+  return(out)
 }
 
 
-#'taxonomize
-#'
-#'
-taxonomize <- function(x, ncbi) {
-  bind_cols(x, as.tibble(
-    getTaxonomy(
-      taxonomizr::accessionToTaxa(
-        x$sseqid, "~/metaxr/data/accessionTaxa.sql"),
-      ncbi[[1]], ncbi[[2]])))
-}
---
+++ R/vsearch_proc.R
@@ -0,0 +1,72 @@
+#' \code{load_blast6} imports blast6-formatted VSEARCH output files from a given directory and merges them into a single tibble
+#'
+#' @param x A file path to directory containing (only) VSEARCH output files
+load_blast6 <- function(x) {
+  map(list.files(path = x,
+                 full.names = TRUE,
+                 ignore.case = TRUE),
+      proc_blast6) %>%
+    bind_rows()
+}
+
+
+#' \code{proc_blast6} processes blast6 input, assigning column names, adding a sample ID field, and extracting accession numbers into their own field
+#'
+#' @param x A VSEARCH blast6 output file
+#' @return A tibble
+proc_blast6 <- function(x) {
+  read_tsv(x,
+           col_names = c("qseqid", "sseqid", "pident",
+                         "length", "mismatch", "gapopen",
+                         "qstart", "qend", "sstart", "send",
+                         "evalue", "bitscore"),
+           guess_max = 100000) %>%
+    mutate(sample = rep(str_extract(x, "CT\\d+"))) %>%
+    separate("sseqid", c("a", "gi", "b", "sseqid", "z"), fill = "warn", sep = "\\|", remove = TRUE) %>%
+    select(sample, qseqid, sseqid, pident, length, gapopen, qstart, qend, sstart, send, evalue, bitscore)
+}
+
+
+#' \code{top_hit} extracts the top ranked subject sequence for each query sequence per sample
+#'
+#' @param x  A tibble produced by \code{load_blast6}
+#' @return A tibble
+top_hit <- function(x) {
+  group_by(x, sample, qseqid) %>% slice(1)
+}
+
+
+#' \code{prepNCBI} prepares the NCBI databases that \code{taxonomizr} needs; this takes some time, so it is better to do it once outside of the functions that use the databases later
+#'
+#' @return A list consisting of NCBI names and nodes
+prepNCBI <- function() {
+  list(taxonomizr::read.nodes('~/metaxr/inst/extdata/nodes.dmp'),
+       taxonomizr::read.names('~/metaxr/inst/extdata/names.dmp'))
+}
+
+
+#'taxonomize
+#'
+#'
+taxonomize <- function(x, db) {
+  bind_cols(x, as.tibble(
+    getTaxonomy(
+      taxonomizr::accessionToTaxa(
+        x$sseqid, "~/metaxr/inst/extdata/accessionTaxa.sql"),
+      db[[1]], db[[2]])))
+}
+
+#'
+#'
+#'
+#'
+tally_genus <- function(x) {
+  x %>% group_by(sample, genus) %>%
+    tally() %>%
+    mutate(prop = n/sum(n)) %>%
+    left_join(x, by = "genus") %>%
+    select(sample = sample.x, family = taxon, prop.x, prop.y, genus, prop) %>%
+    distinct()
+  # %>%
+  #   select(sample, family = taxon, prop.x, prop.y, genus, count = n, prop)
+}
