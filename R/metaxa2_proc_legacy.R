e3a9cab7c948e86c5754c043f90a71d925243ace
Doug Sponsler <sponslerdb@gmail.com>
2018-08-30 21:49:08 -0400

    added eval procedure

--- R/metaxa2_proc_legacy.R
+++ R/metaxa2_proc_legacy.R
@@ -1,13 +1,13 @@
 #' \code{metaxize} converts Metaxa2 output files to a single tibble expressing proportional abundance of reads at the family level
 #'
 #' @param x A filepath to a directory containing (only) taxonomy.txt files
+#' @param min_prop A real number specifying the minumum proportional abundance below which a taxon will be dropped as a likely false positive
 #' @return A tibble of sample-wise taxonomic proportions
-metaxize <- function(x, regext = "CT\\d+") {
+metaxize <- function(x, regext = "CT\\d+", min_prop = 0.0001) {
   # list files in source directory
   files <- list.files(path = x,
                       full.names = TRUE,
                       ignore.case = TRUE)
-
   # process by sample
   tlist <- map(files, function(y) {
     read_tsv(y, col_names = c("ID", "tax_tree", "perID", "length", "relscore")) %>%
@@ -19,72 +19,71 @@ metaxize <- function(x, regext = "CT\\d+") {
       summarise(reads = n()) %>%
       mutate(prop = reads/sum(reads),
              family = str_remove(family, ".__"),
-             family = replace_na(family, "UNDETERMINED"),
+             family = replace_na(family, "*undetermined*"),
+             family = factor(family),
              sample = rep(str_extract(y, regext))) %>%
       select(sample, family, prop)
   }
   )
-
   # combine samples into single output tibble
-  out <- bind_rows(tlist) %>%
-    filter(family == "UNDETERMINED" | prop >= 0.001 )
+  out <- bind_rows(tlist) %>% # collate sample-wise tibbles
+    filter(family == "*undetermined*" | prop >= min_prop ) # trim to taxa comprising at least 0.1% of reads, but retain undetermined reads regardless of rarity
 
   return(out)
 }
 
 
-#' \code{consensus_xyz_fam} returns taxa common to at least two of datasets x, y, and z
+#' \code{consensus_xyz} returns taxa common to at least two of the marker datasets x, y, and z; reports median proportional read count
 #'
 #' @param x,y,z Tibbles output from \code{metaxize}
-#' @return
-consensus_xyz <- function(x, y, z) {
+#' @param min_prop A real number specifying the minumum proportional abundance below which a taxon will be dropped as a likely false positive
+#' @return a tibble
+consensus_xyz <- function(x, y, z, min_prop) {
+  # taxa common to markers x and y
   xy <- inner_join(x, y, by = c("sample", "family"))
   colnames(xy) <- c("sample", "family",
                     paste(deparse(substitute(x)), "prop", sep = "."),
                     paste(deparse(substitute(y)), "prop", sep = "."))
+  # taxa common to markers x and z
   xz <- inner_join(x, z, by = c("sample", "family"))
   colnames(xz) <- c("sample", "family",
                     paste(deparse(substitute(x)), "prop", sep = "."),
                     paste(deparse(substitute(z)), "prop", sep = "."))
+  # taxa common to markers y and z
   yz <- inner_join(y, z, by = c("sample", "family"))
   colnames(xz) <- c("sample", "family",
                     paste(deparse(substitute(y)), "prop", sep = "."),
                     paste(deparse(substitute(z)), "prop", sep = "."))
-
+  # taxa common to any two of x, y, and z
   sieve <- xy %>%
     full_join(xz, by = c("sample", "family")) %>%
     full_join(yz, by = c("sample", "family")) %>%
     select(sample, family)
-
+  # filter original marker datasets by the taxa in the sieve dataset created above
   x_sieve <- semi_join(x, sieve, b = c("sample", "family"))
-
   y_sieve <- semi_join(y, sieve, b = c("sample", "family"))
-
   z_sieve <- semi_join(z, sieve, b = c("sample", "family"))
-
+  # join filtered marker datasets
   out <- full_join(x_sieve, y_sieve, by = c("sample", "family")) %>%
     full_join(z_sieve, by = c("sample", "family"))
-
-  colnames(out) <- c("sample", "family",
-                     "X.prop",
-                     "Y.prop",
-                     "Z.prop")
-
-
+  # rename columns
+  colnames(out) <- c("sample", "family", "X.prop", "Y.prop", "Z.prop")
+  # calculate median read count proportions
   out <- out %>%
     rowwise() %>%
     mutate(med_prop = median(c(X.prop,
                                Y.prop,
                                Z.prop), na.rm = TRUE)) %>%
-    filter(family == "UNDETERMINED" | med_prop >= 0.001)
-
-
+    filter(family == "*undetermined*" | med_prop >= min_prop) %>% # filter to exclude taxa with a median proportional abundance of less than 0.1%
+    group_by(sample) %>%
+    mutate(scaled_prop = med_prop*(1/sum(med_prop))) # rescale median proportional abundance so that it totals to 1 for each sample
+  # rename columns to bear marker names
   colnames(out) <- c("sample", "family",
                      paste(deparse(substitute(x)), "prop", sep = "."),
                      paste(deparse(substitute(y)), "prop", sep = "."),
                      paste(deparse(substitute(z)), "prop", sep = "."),
-                     "med_prop")
-
+                     "med_prop",
+                     "scaled_prop")
   return(out)
 }
 
@@ -92,12 +91,12 @@ consensus_xyz <- function(x, y, z) {
 #' \code{add_meta} joins a set of metadata to data by sample field
 #'
 #' @param x a filepath to metadata file containing site field shared with data
-#' @return
+#' @return a tibble containing site, hive, and date for each sample
 add_meta <- function(x) {
   key <- read_csv("~/metaxr/inst/extdata/CT_sample_key.csv") %>%
     select(sample, colony, site, date)
   full_join(x, key, by = "sample") %>%
-    mutate(date = lubridate::as_date(date)) %>%
+    mutate(date = lubridate::as_date(date),) %>%
     select(sample, colony, site, date, everything()) %>%
     arrange(site, date)
 }
@@ -109,33 +108,114 @@ add_meta <- function(x) {
 #' @param fam family data set
 #' @return left_join of family and genus such that all families are retained but only genera with family-level hits are retained
 consensus_fg <- function(gen, fam) {
-  left_join(fam, gen, by = c("sample", "family")) %>%
-    select(-prop)
+  left_join(fam, gen, by = c("sample", "family"))
 }
 
 
-#' \code{metaxeval_leg} reports classification rates
+#' \code{read_count} reports total number of reads for each library
 #'
 #' @param x A filepath to a directory containing (only) taxonomy-reliability.txt files
-#' @param tlev A string specifying the taxonomic level at which results should be aggregated
-#' @param rscore A number between 0 and 100 specifying the relaiability score cutoff; only reads having a reliability score >= \code{rscore} will pass filter
-#' @return a tibble with raw read count, classified read count (reads passing rel_score filter), and classification rate
-
-metaxeval <- function(x, regext = "CT\\d+") {
+#' @return a tibble showing raw read count by library
+read_count <- function(x, regext = "CT\\d+") {
   files <- list.files(path = x,
                       full.names = TRUE,
                       ignore.case = TRUE)
   tlist <- map(files, function(y) {
-    read_tsv(y, col_names = c("ID", "tax_tree", "perID", "length", "relscore")) %>%
-      separate(tax_tree, c("kingdom", "phylum", "class", "order",
-                           "family", "genus", "species", "x", "y"),
-               fill = "warn", sep = ";", remove = TRUE) %>%
+    read_tsv(y) %>%
       summarise(sequences = n()) %>%
       mutate(sample = rep(str_extract(y, regext))) %>%
       select(sample, sequences)
   }
   )
-  out <- bind_rows(tlist)
+  out <- bind_rows(tlist) %>%
+    add_meta()
   return(out)
 }
 
+
+
+
+
+
+metax_count <- function(x, regext = "CT\\d+", marker) {
+  # list files in source directory
+  files <- list.files(path = x,
+                      full.names = TRUE,
+                      ignore.case = TRUE)
+  # process by sample
+  tlist <- map(files, function(y) {
+    read_tsv(y, col_names = c("ID", "tax_tree", "perID", "length", "relscore")) %>%
+      separate(tax_tree, c("kingdom", "phylum", "class", "order",
+                           "family", "genus", "species", "x", "y"),
+               fill = "warn", sep = ";", remove = TRUE) %>%
+      select(ID, family) %>%
+      mutate(family = str_remove(family, ".__"),
+             family = replace_na(family, "*undetermined*"),
+             family = factor(family),
+             sample = rep(str_extract(y, regext))) %>%
+      select(sample, family)
+  }
+  )
+  # combine samples into single output tibble
+  det_reads <- bind_rows(tlist) %>% # collate sample-wise tibbles
+    filter(family != "*undetermined*") %>%
+    group_by(sample) %>%
+    summarize(reads_to_fam = n())
+
+  all_reads <- bind_rows(tlist) %>% # collate sample-wise tibbles
+    group_by(sample) %>%
+    summarize(reads = n())
+
+  out <- full_join(all_reads, det_reads, by = "sample") %>%
+    mutate(fam_class_rate = reads_to_fam/reads)
+
+  colnames(out) <- c("sample",
+                     paste(deparse(substitute(marker)), "reads", sep = "."),
+                     paste(deparse(substitute(marker)), "reads_to_fam", sep = "."),
+                     paste(deparse(substitute(marker)), "fam_class_rate", sep = "."))
+  return(out)
+}
+
+
+
+tax_freq <- function(x, tax) {
+
+  tax <- enquo(tax)
+
+  samples <- x %>%
+    select(site, sample) %>%
+    group_by(site) %>%
+    summarise(samples = n_distinct(sample))
+
+  detections <- x %>%
+    group_by(!!tax, site) %>%
+    summarise(detections = n_distinct(sample)) %>%
+    arrange(-detections)
+
+  tax_freq <- full_join(samples, detections, by = "site") %>%
+    mutate(freq = detections/samples) %>%
+    arrange(-freq, site) %>%
+    filter(!!tax != "*undetermined*") %>%
+    select(site, !!tax, samples, detections, freq)
+}
+
+tax_abund <- function(x, tax, prop) {
+  tax <- enquo(tax)
+  prop <- enquo(prop)
+
+  samples <- x %>%
+    select(site, sample) %>%
+    group_by(site) %>%
+    summarise(samples = n_distinct(sample))
+
+  abund <- x %>%
+    select(sample, site, !!tax, !!prop) %>%
+    distinct()
+
+  abund_summary <- full_join(samples, abund, by = "site") %>%
+    group_by(site, family) %>%
+    summarise(mean_abund = sum(scaled_prop)/unique(samples),
+              max_abund = max(scaled_prop)) %>%
+    filter(!!tax != "*undetermined*") %>%
+    arrange(-mean_abund)
+}
--- R/vsearch_proc.R
+++ R/vsearch_proc.R
@@ -1,6 +1,7 @@
 #' \code{load_blast6} imports blast6-formatted VSEARCH output files from a given directory and merges them into a single tibble
 #'
 #' @param x A file path to directory containing (only) VSEARCH output files
+#' @return A tibble
 load_blast6 <- function(x) {
   map(list.files(path = x,
                  full.names = TRUE,
@@ -15,14 +16,13 @@ load_blast6 <- function(x) {
 #' @param x A VSEARCH blast6 output file
 #' @return A tibble
 proc_blast6 <- function(x) {
-  read_tsv(x,
-           col_names = c("qseqid", "sseqid", "pident",
-                         "length", "mismatch", "gapopen",
-                         "qstart", "qend", "sstart", "send",
-                         "evalue", "bitscore"),
-           guess_max = 100000) %>%
-    mutate(sample = rep(str_extract(x, "CT\\d+"))) %>%
-    separate("sseqid", c("a", "gi", "b", "sseqid", "z"), fill = "warn", sep = "\\|", remove = TRUE) %>%
+  read_tsv(x, col_names = c("qseqid", "sseqid", "pident", # read in file and add column names
+                            "length", "mismatch", "gapopen",
+                            "qstart", "qend", "sstart", "send",
+                            "evalue", "bitscore"),
+           guess_max = 100000) %>% # it is necessary to increase guess_max so that it will assign the correct data type and avoid parsing errors
+    mutate(sample = rep(str_extract(x, "CT\\d+"))) %>% # add sample field
+    separate("sseqid", c("a", "gi", "b", "sseqid", "z"), fill = "warn", sep = "\\|", remove = TRUE) %>% # pull out accession number from sseqid field
     select(sample, qseqid, sseqid, pident, length, gapopen, qstart, qend, sstart, send, evalue, bitscore)
 }
 
@@ -45,28 +45,47 @@ prepNCBI <- function() {
 }
 
 
-#'taxonomize
+#' \code{taxonomize} is a wrapper for the taxonomizr::getTaxonomy and taxonomizr::accessionToTaxa; assigns taxonomic data to NCBI accession numbers using the data base creates by \code{prepNCBI}
 #'
-#'
+#' @param x A tibble produced by \code{load_blast6} and \code{top_hit}
+#' @param db A data base created by \code{prepNCBI}
+#' @return A tibble
 taxonomize <- function(x, db) {
   bind_cols(x, as.tibble(
-    getTaxonomy(
+    taxonomizr::getTaxonomy(
       taxonomizr::accessionToTaxa(
         x$sseqid, "~/metaxr/inst/extdata/accessionTaxa.sql"),
       db[[1]], db[[2]])))
 }
 
+#' \code{tally_gen} sums reads by genus for each sample, calculates proportional abundance, and culls rare genera whose proportional abundance is less than min_prop
 #'
-#'
-#'
-#'
-tally_gen <- function(x) {
+#' @param x A tibble produced by \code{taxonomize}
+#' @param min_prop A real number specifying the minumum proportional abundance below which a taxon will be dropped as a likely false positive
+#' @return A tibble
+tally_gen <- function(x, min_prop = 0.001) {
   x %>% group_by(sample, genus) %>%
-    tally() %>%
-    mutate(prop = n/sum(n)) %>%
-    left_join(x, by = c("sample", "genus")) %>%
-    select(sample, family, genus, prop) %>%
-    filter(prop >= 0.001) %>%
-    distinct() %>%
-    na.omit()
+    tally() %>% # sum reads by genus within each sample
+    mutate(gen_prop = n/sum(n)) %>% # turn counts to proportions
+    left_join(x, by = c("sample", "genus")) %>% # join summary data back into original data frame by sample and genus
+    select(sample, family, genus, gen_prop) %>%
+    filter(gen_prop >= min_prop) %>% # remove genera falling below min_prop in abundance
+    distinct() %>% # remove duplicate rows created by the left_join
+    na.omit # hits to accession numbers that do not have taxonomic data at family and genus level (these are rare) are omitted
 }
+
+
+
+vsearch_eval <- function(x) {
+  x %>%
+    group_by(sample) %>%
+    summarize(ITS2.reads_to_gen = n()) %>%
+    add_meta
+}
+
+# ITS2_gen_class_rate <- add_meta(ITS2_vsearch) %>%
+#   group_by(sample) %>%
+#   summarize(reads_classified = n()) %>%
+#   full_join(filter(read_summary, marker == "ITS2_reads"), by = "sample") %>%
+#   mutate(class_rate = reads_classified/reads) %>%
+#   select(sample, site, date, reads, reads_classified, class_rate)
